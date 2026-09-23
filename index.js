8c6352b5a1f2d69d457ae2ce0f26b4ea7b3c2f63
Juan Rodrigues <juanjrg2114@gmail.com>
2026-09-23 19:39:20 -0300

    Criando a secao criar conta

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //moulo externos
 const { default: inquirer } = require("inquirer")
-const chalk = require('chalk')
+const { default: chalk } = require('chalk')
 
 //modulos internos
 const fs = require('fs')
@@ -14,7 +14,19 @@ function operation() {
             message: 'O que você deseja fazer?',
             choices: ['Criar Conta', 'Consultar Salso', 'Depositar', 'Sacar', 'Sair'],
         },
-    ]).then().catch((err) => { console.log(err) })
+    ]).then((answer) => {
+        const action = answer['action']
+        if (action === 'Criar Conta') {
+            createAccount()
+        }
+    })
+        .catch((err) => { console.log(err) })
+}
+
+//create account 
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
 }
 
 
