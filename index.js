1614fcd871e7ac93be66b5d32c6985c3c32d53d1
Juan Rodrigues <juanjrg2114@gmail.com>
2026-09-23 19:26:57 -0300

    Adicionado funcao que inicializa as opcoes no terminal

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const { default: inquirer } = require("inquirer")
 const chalk = require('chalk')
 
 //modulos internos
+const fs = require('fs')
+operation()
+
+function operation() {
+    inquirer.prompt([
+        {
+            type: 'select',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: ['Criar Conta', 'Consultar Salso', 'Depositar', 'Sacar', 'Sair'],
+        },
+    ]).then().catch((err) => { console.log(err) })
+}
+
+
+
+
+
+
