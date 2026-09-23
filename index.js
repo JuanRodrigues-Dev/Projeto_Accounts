22684ccc86a004803612822bf2b5f6efb230ad4f
Juan Rodrigues <juanjrg2114@gmail.com>
2026-09-23 19:59:08 -0300

    Criando funcoes que salva o nome na criação da conta, e verifica se usuario ja existe

--- index.js
+++ index.js
@@ -27,6 +27,38 @@ function operation() {
 function createAccount() {
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
     console.log(chalk.green('Defina as opções da sua conta a seguir'))
+    buildAccount()
+}
+
+function buildAccount(){
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite seu Nome para sua Conta:'
+        },
+    ]).then((answer)=>{
+        const accountName = answer['accountName']
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Esta Conta já existe , escolha outro nome!'))
+            buildAccount()
+            return
+        }
+        fs.writeFileSync(`accounts/${accountName}.json`, '"balance":0',
+            function(err){
+                console.log(err)
+            }
+        )
+        console.log(chalk.green('Parabéns , Conta criada com sucesso!'))
+        operation()
+
+    })
+    .catch((err)=>{console.log(err)})
 }
 
 
